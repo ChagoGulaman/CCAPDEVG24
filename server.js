80bbe0a63ca0375e3953eee9b37baa15b586ac07
ChagoGulaman <119985818+ChagoGulaman@users.noreply.github.com>
2022-12-08 16:29:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -393,4 +393,4 @@ app.get('/tagaytay', (req, res) => {
 });
 
 //PORT IS 3000
-app.listen(3000, () => console.log('listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000!'));
